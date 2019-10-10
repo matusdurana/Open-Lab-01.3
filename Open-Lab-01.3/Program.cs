635a018b60e947070131fbeb4e1e760c7802c387
MatusDurana <matusdurana2mail.com>
2019-10-10 09:22:02 +0200

    Add project files.

---
+++ Open-Lab-01.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Open_Lab_01._3
+{
+    class Program
+    {
+        public static int Addition(int num)
+
+        {
+            return (num + 1);
+        }
+
+        static void Main(String[] args)
+        {
+            int num;
+            int addition;
+
+            Console.WriteLine("Zadajte svoje číslo:");
+            num = int.Parse(Console.ReadLine());
+
+            addition = Addition(num);
+            Console.WriteLine(num);
+        }
+    }
+}
